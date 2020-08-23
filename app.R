0d9afd46b8816b4f7d364196f6c6fabe0576a368
Mathias Dillen <37537540+matdillen@users.noreply.github.com>
2020-08-23 13:05:28 +0200

    add package install

--- app.R
+++ app.R
@@ -1,3 +1,29 @@
+pkgLoad <- function() {
+    
+    packages <- c("DT",
+                   "tidyverse",
+                   "magrittr",
+                   "shiny",
+                   "shinyjs")
+    packagecheck <- match( packages, utils::installed.packages()[,1])
+    packagestoinstall <- packages[is.na(packagecheck)]
+    
+    if(length( packagestoinstall) > 0 ) {
+        utils::install.packages(packagestoinstall)
+    } else {
+        print("All requested packages already installed")
+    }
+    
+    for(package in packages) {
+        suppressPackageStartupMessages(
+            library(package,
+                    character.only = T,
+                    quietly = T))
+    }
+}
+
+pkgLoad()
+
 library(tidyverse)
 library(magrittr)
 library(shiny)
